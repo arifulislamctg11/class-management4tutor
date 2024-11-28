c93b9c2076ba6a9e72ef8a89a75a2888ed769cbb
tanjimN4 <tanjimahemed7@gmail.com>
2024-11-28 17:12:40 +0600

    signup

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -8,14 +8,28 @@ import Link from 'next/link';
 const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 import signup_Animation from "../../../public/assets/Animation.json";
+import axios from 'axios';
 
 const Signup = () => {
     const { register, handleSubmit, formState: { errors }, watch } = useForm();
     const [loading, setLoading] = useState(false);
 
-    const onSubmit = (data) => {
-        console.log(data);
+    const onSubmit = async (data) => {
+        console.log("Form Data:", data);
         // handle your form submission logic here
+        try {
+            const response = await axios.post('signup/api', data);
+            if (response.status === 201) {
+                console.log("User created successfully:", response.data);
+                // Handle success (e.g., redirect to login page)
+            } else {
+                console.error("Error:", response.data.error);
+                alert(response.data.error || "An error occurred");
+            }
+        } catch (error) {
+            console.error("Error during signup:", error);
+            alert("An unexpected error occurred");
+        }
     };
 
     const handleGoogleSignUp = () => {

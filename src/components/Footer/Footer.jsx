6d8e91751ff3c7499ba48409101dfcda86b39657
OMI786I <sohrawardy1998@gmail.com>
2024-11-29 19:49:32 +0600

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,11 +9,6 @@ const Footer = () => {
             href="https://flowbite.com/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               className Management System 4 Tutor
             </span>
